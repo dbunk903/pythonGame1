3ddec8a969c7bf92460cb6c6c7fa6999b77bc24c
dbunk903 <dbunk903@gmail.com>
2022-02-20 19:56:32 +0900

    Handled FPS and characterspeed

--- pygame_basic/main.py
+++ pygame_basic/main.py
@@ -8,9 +8,14 @@ screen_width = 480  # 가로 크기
 screen_height = 640 # 세로 크기 
 screen = pygame.display.set_mode((screen_width,screen_height))
 
-
 # 화면 타이틀 설정
 pygame.display.set_caption("BUBBLE BUBBLE") # 게임 이름
+
+# FPS
+clock = pygame.time.Clock()
+
+# 이동속도
+character_speed = 0.6
 
 # 배경 이미지 불러오기
 background = pygame.image.load('/Users/jeroz/Library/Mobile Documents/com~apple~CloudDocs/Documents/AllCode/NaDo/pythonGame/pygame_basic/pythonGameBackground.png')
@@ -30,7 +35,8 @@ to_y = 0
 # 이벤트 루프
 running = True # 게임이 진행중인가? 
 while running:
-
+    dt = clock.tick(60) # delta , 게임 화면의 초당 프레임 수를 설정 
+    # 확인 print("fps : " + str(clock.get_fps()))
     for event in pygame.event.get(): # 어떤 이벤트가 발생하였는가?
         if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하였는가? 
             running = False # 게임이 진행중 아님...
@@ -41,21 +47,21 @@ while running:
         '''
         if event.type == pygame.KEYDOWN: # 키가 눌러 졌는지 확인
             if event.key == pygame.K_LEFT: # character goes left
-                to_x -= 20
+                to_x -= character_speed
             elif event.key == pygame.K_RIGHT: # character goes right
-                to_x += 20
+                to_x += character_speed
             elif event.key == pygame.K_UP: # character goes up
-                to_y -= 20
+                to_y -= character_speed
             elif event.key == pygame.K_DOWN: # character goes down
-                to_y += 20
+                to_y += character_speed
         if event.type == pygame.KEYUP: # 키보드 입력값이 있다가 없는 경우 
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 to_x = 0
             elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 to_y = 0
 
-        character_x_pos += to_x
-        character_y_pos += to_y
+        character_x_pos += to_x * dt
+        character_y_pos += to_y * dt 
         
         # 가로 경계값 처리 
         if character_x_pos < 0:
